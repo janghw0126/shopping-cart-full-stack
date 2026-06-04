0c32e4972b2695fe2b3978a379b02aaba59a8dc1
janghw0126 <janghw232625@gmail.com>
2026-06-04 21:39:52 +0900

    fix: GetCartApi에서 오류 처리 방식 수정

--- client/src/api/cartApi.ts
+++ client/src/api/cartApi.ts
@@ -3,9 +3,10 @@ import type { CartItem } from "../interface/cart";
 export async function GetCartApi(
   setLoading: (value: boolean) => void,
   setCartItems: (value: CartItem[]) => void,
+  setError: (value: string) => void,
 ) {
   const res = await fetch("/carts");
-  if (!res.ok) throw new Error(`HTTP error: ${res.status}`);
+  if (!res.ok) return setError(`HTTP error: ${res.status}`);
   const response = await res.json();
   const { status, data } = response;
 
@@ -13,6 +14,6 @@ export async function GetCartApi(
     setCartItems(data);
     setLoading(false);
   } else {
-    throw new Error("장바구니 상품 목록을 성공적으로 불러오지 못했습니다.");
+    setError("장바구니 상품 목록을 성공적으로 불러오지 못했습니다.");
   }
 }
