d2ac0043b24690bc4c90e9c554a260b78661f9d2
janghw0126 <janghw232625@gmail.com>
2026-06-04 21:40:31 +0900

    feat: 장바구니 API 오류 처리 및 로딩 상태에 따른 컴포넌트 추가

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,30 @@
 import { useEffect, useState } from "react";
 import { GetCartApi } from "./api/cartApi";
+import { Spinner } from "./common/Spinner";
 import type { CartItem } from "./interface/cart";
+import { ErrorMessage } from "./common/ErrorMessage";
+import { EmptyCart } from "./CartPage/EmptyCart";
+import { CartList } from "./CartPage/CartList";
 
 function App() {
+  const [error, setError] = useState("");
   const [loading, setLoading] = useState(true);
   const [cartItems, setCartItems] = useState<CartItem[]>([]);
 
   // GET/carts 호출해서 장바구니 api 가져오기
   useEffect(() => {
-    GetCartApi(setLoading, setCartItems);
+    GetCartApi(setLoading, setCartItems, setError);
   }, []);
 
-  return loading ? "로딩중" : (cartItems===""? 상품 없음 : </>);
+  return loading ? (
+    <Spinner />
+  ) : error ? (
+    <ErrorMessage message={error} />
+  ) : cartItems.length !== 0 ? (
+    <CartList cartItems={cartItems} />
+  ) : (
+    <EmptyCart />
+  );
 }
 
 export default App;
