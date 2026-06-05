8392d7be4bc825e926a048ebe499713b80cbe59a
janghw0126 <janghw232625@gmail.com>
2026-06-05 16:49:05 +0900

    refactor: ErrorMessage 스타일을 CSS 파일에서 styled-components로 변경

--- client/src/common/ErrorMessage.tsx
+++ client/src/common/ErrorMessage.tsx
@@ -1,9 +1,21 @@
-import "./ErrorMessage.css";
+import styled from "@emotion/styled";
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+`;
+
+const Message = styled.p`
+  color: #d32f2f;
+  font-size: 16px;
+`;
 
 export function ErrorMessage({ message }: { message: string }) {
   return (
-    <div className="error-message">
-      <p>{message}</p>
-    </div>
+    <Wrapper>
+      <Message>{message}</Message>
+    </Wrapper>
   );
 }
