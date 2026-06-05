453aee87b14ee316311ef68939da7a331299c994
janghw0126 <janghw232625@gmail.com>
2026-06-05 16:08:52 +0900

    feat: Spinner 컴포넌트를 styled-components로 리팩토링

--- client/src/common/Spinner.tsx
+++ client/src/common/Spinner.tsx
@@ -1,5 +1,30 @@
-import "./Spinner.css";
+import styled from "@emotion/styled";
+import { keyframes } from "@emotion/react";
+
+const spin = keyframes`
+  to { transform: rotate(360deg); }
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+`;
+
+const Circle = styled.div`
+  width: 30px;
+  height: 30px;
+  border: 4px solid #e0e0e0;
+  border-top-color: #333;
+  border-radius: 50%;
+  animation: ${spin} 0.8s linear infinite;
+`;
 
 export function Spinner() {
-  return <div className="spinner" />;
+  return (
+    <Wrapper>
+      <Circle />
+    </Wrapper>
+  );
 }
