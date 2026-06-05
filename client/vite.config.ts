45da8f2eef164bb9fb4ecf3b9cedbfd45a4805d2
janghw0126 <janghw232625@gmail.com>
2026-06-05 16:02:23 +0900

    feat: package.json 및 vite.config.ts에 테스트 관련 의존성 및 설정 추가

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
@@ -10,5 +11,10 @@ export default defineConfig({
       "/carts":
         "https://shopping-cart-full-stack-production-0bda.up.railway.app",
     },
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/test/setup.ts",
   },
 });
