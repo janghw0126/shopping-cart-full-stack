f5da2f4738c5173ea3e49f05a7f9dbaa039e3e33
janghw0126 <janghw232625@gmail.com>
2026-06-22 03:50:26 +0900

    feat(fe): 결제 API 프록시 추가

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -12,6 +12,8 @@ export default defineConfig({
         "https://shopping-cart-full-stack-production-0bda.up.railway.app",
       "/orders":
         "https://shopping-cart-full-stack-production-0bda.up.railway.app",
+      "/payments":
+        "https://shopping-cart-full-stack-production-0bda.up.railway.app",
     },
   },
   test: {
