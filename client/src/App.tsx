45e3e5375dd8cd40bad78f7a969ac41893750d6b
janghw0126 <janghw232625@gmail.com>
2026-06-21 13:28:36 +0900

    refactor(fe): 결제 금액 확인/주문 확인 페이지 컴포넌트 분리

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import { OrderCheck } from "./CheckoutPage/OrderCheck";
+import { PaymentConfirm } from "./CheckoutPage/PaymentConfirm";
 import { CartPage } from "./CartPage/CartPage";
 
 function App() {
   return (
     <Routes>
       <Route path="/cart" element={<CartPage />} />
-      <Route path="/checkout" element={<OrderCheck />} />
+      <Route path="/checkout/:orderId" element={<OrderCheck />} />
+      <Route path="/payment/confirm" element={<PaymentConfirm />} />
       <Route path="*" element={<Navigate to="/cart" replace />} />
     </Routes>
   );
